0636c681d34da1b122f636b5f80220ad5e462f18
S. Bruce <28817345+foundObjects@users.noreply.github.com>
2019-05-03 20:43:58 -0700

    WIP Commit -- lots of code changes to the scripts, testing to determine what's causing some lock issues with the zram volume on service stop at the moment

--- install.sh
+++ install.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
+set -ex
 
-# set -ex
-
-systemctl -q is-active log2zram  && { echo "ERROR: log2zram service is still running. Please run \"sudo service log2zram stop\" to stop it and uninstall"; exit 1; }
-[ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
-[ -d /usr/local/bin/log2zram ] && { echo "Log2Zram is already installed, uninstall first"; exit 1; }
+[ "$(id -u)" -eq 0 ] ||
+  {
+    echo "You need to be ROOT (sudo can be used)"
+    exit 1
+  }
+( systemctl -q is-active log2zram ) &&
+  {
+    echo 'ERROR: log2zram service is still running.'
+    echo 'Please run "sudo service log2zram stop" to stop it and then uninstall' \
+      'before re-installing.'
+    exit 1
+  }
+[ -e /usr/local/bin/log2zram ] &&
+  {
+    echo "log2zram is already installed, uninstall first"
+    exit 1
+  }
 
 # save time (if we can) on slow machines/storage loading and parsing apt package lists
-( dpkg -l | grep -q "libattr1-dev" ) || \
-	apt-get install libattr1-dev -y
-[ -d overlayfs-tools ] || \
-	git clone https://github.com/kmxz/overlayfs-tools
+( dpkg -l | grep -q "libattr1-dev" ) || apt-get install libattr1-dev -y
+# only pull if neccessary, otherwise use local copy if available
+[ -d overlayfs-tools ] || git clone https://github.com/kmxz/overlayfs-tools
 cd overlayfs-tools
 make
 cd ..
 
-mkdir -p /usr/local/share/log2zram/
-# log2zram install 
+# log2zram install
+mkdir -p /usr/local/share/log2zram/ /usr/local/lib/log2zram/
 install -m 644 log2zram.service /etc/systemd/system/log2zram.service
 install -m 755 log2zram /usr/local/bin/log2zram
-install -m 644 log2zram.conf /etc/log2zram.conf
+install -m 644 log2zram.conf /etc/default/log2zram
 install -m 644 uninstall.sh /usr/local/share/log2zram/uninstall.sh
 install -m 644 log2zram.logrotate /etc/logrotate.d/00_log2zram
-mkdir -p /usr/local/lib/log2zram/
 install -m 755 overlayfs-tools/overlay /usr/local/lib/log2zram/overlay
-mkdir -p /usr/local/share/log2zram/log
 systemctl enable log2zram
-
--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+#set -ex
 
-if [ "$(id -u)" -eq 0 ]
-then
-	service log2zram stop
-	systemctl disable log2zram
-	rm /etc/systemd/system/log2zram.service
-	rm /usr/local/bin/log2zram
-	rm /etc/log2zram.conf
-	rm /etc/logrotate.d/00_log2zram
-	echo "Log2Zram is uninstalled, removing the uninstaller in progress"
-	rm -rf /usr/local/share/log2zram
-	rm -rf /usr/local/lib/log2zram
-	echo "##### Reboot isn't needed #####"
-else
-	echo "You need to be ROOT (sudo can be used)"
-fi
+[ "$(id -u)" -eq 0 ] ||
+  {
+    echo "You need to be ROOT (sudo can be used)"
+    exit 1
+  }
+
+systemctl -q is-active log2zram > /dev/null 2>&1 && systemctl stop log2zram
+systemctl -q is-enabled log2zram > /dev/null 2>&1 && systemctl disable log2zram
+rm /etc/systemd/system/log2zram.service
+rm /usr/local/bin/log2zram
+rm /etc/default/log2zram
+rm /etc/logrotate.d/00_log2zram
+rm -f /var/run/log2zram.device
+echo "log2zram is uninstalled, removing the uninstaller in progress"
+rm -rf /usr/local/share/log2zram /usr/local/lib/log2zram
+echo "##### Reboot isn't needed #####"
